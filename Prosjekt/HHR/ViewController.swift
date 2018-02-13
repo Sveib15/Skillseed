9eee9fb849b4686c79ea23b0e76091817fb646a3
Svei Bruce <sveinbruce132@gmail.com>
2018-02-13 12:48:10 +0100

    GUI PROTOTYPE: Made a new storyboard and added multiple views. The views are "Settings", "Edit skill", "Edit Description", "Certification" and "About"

--- Prosjekt/HHR/ViewController.swift
+++ Prosjekt/HHR/ViewController.swift
@@ -9,7 +9,6 @@
 import UIKit
 
 class ViewController: UIViewController {
-//Test4. xcode Svein 2
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
