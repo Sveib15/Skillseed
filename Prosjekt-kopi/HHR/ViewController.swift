d953461085950146677ba9b2f1b7e9de5ed4b3ec
Svei Bruce <sveinbruce132@gmail.com>
2018-02-15 12:03:40 +0100

    GUI PROTOTYPE: pushed for security 10

---
+++ Prosjekt-kopi/HHR/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  HHR
+//
+//  Created by Svein Bruce on 09.02.2018.
+//  Copyright © 2018 Helping Hand. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
