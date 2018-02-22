0613f50982cce619f37cc3a077f1f68825222b68
Svei Bruce <sveinbruce132@gmail.com>
2018-02-22 13:51:21 +0100

    Made new CalendarViewController file as the old one wasn't recognized

---
+++ Prosjekt-kopi/HHR/CalenderViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CalendarVC.swift
+//  HHR
+//
+//  Created by Anders Berntsen on 13.02.2018.
+//  Copyright © 2018 Helping Hand. All rights reserved.
+//
+
+
+import UIKit
+
+class CalendarVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func closePopup(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    //nei
+    
+    
+    
+    
+    
+}
